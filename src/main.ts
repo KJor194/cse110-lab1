5f47e2254f5124921ae913efbd946b72ece8c3dc
Kyle J <kjorrin@ucsd.edu>
2025-10-02 19:21:07 -0700

    Added animation functionality, lemon center fixed

--- src/main.ts
+++ src/main.ts
@@ -14,10 +14,9 @@ stage.add(layer);
 // create our shape
 
 const lemon = new Konva.Line({
-  offsetX: 220,
-  offsetY: 290,
-  points: [236, 330, 259, 310, 270, 307, 281, 310, 304, 330, 281, 350, 270, 353, 259, 350],
-  //points: [336, 430, 359, 410, 370, 407, 381, 410, 404, 430, 381, 450, 370, 453, 359, 450],
+  x: 400,
+  y: 300, 
+  points: [336, 430, 359, 410, 370, 407, 381, 410, 404, 430, 381, 450, 370, 453, 359, 450],
   fill: '#FFFF00',
   stroke: 'black',
   strokeWidth: 5,
@@ -25,47 +24,59 @@ const lemon = new Konva.Line({
   tension: 0.3,
 });
 
-// Adding events to the lemon
+//Moving center of lemon from origin to its center:
+const box = lemon.getClientRect({ relativeTo: lemon }); 
+lemon.offsetX(box.width / 2 + box.x);
+lemon.offsetY(box.height / 2 + box.y);
 
+// Adding events to the mouse
+//When the mouse is over the lemon, the mouse cursor turns into pointer:
 lemon.on('mouseover', function (e) {
   e.target.getStage().container().style.cursor = 'pointer';
 });
 
+//When the mouse is off of the lemon, the mouse cursor turns back to the default cursor:
 lemon.on('mouseout', function (e) {
   e.target.getStage().container().style.cursor = 'default';
 });
 
+//Adding events to the lemon
+//Animating the oscillation
 
-//const animation = new Konva.Animation(function(frame){
+const amplitude = 10;
+const speed = 0.05;
 
-//}, layer);
+const animation = new Konva.Animation((frame) => {
+  // frame.time is total elapsed time in ms
+  lemon.rotation(amplitude * Math.sin(frame.time * speed));
+}, layer);
 
-const ogState = {
-  scaleX: lemon.scaleX(),
-  scaleY: lemon.scaleY(),
-  rotation: lemon.rotation(),
-  fill: lemon.fill()
-};
+//Recording original orientation:
 
-const clickScale = 1.5;  // scale up by 50% on click
+const og_orientation = lemon.rotation();
 
+//Scaling up by 50% on mousedown
+const clickScale = 1.5;
+
+//On mousedown, the lemon oscillates, is draggable, and scales up
 lemon.on('mousedown', () => {
-  lemon.draggable(true); // allow dragging while held
+  lemon.draggable(true);
   lemon.scale({x: 1.5, y: 1.5});
+  animation.start();
 });
 
+//On mouseup, the lemon stops oscillating, reverts to its original orientation, can no longer be dragged, and scales back to original size
 lemon.on('mouseup', () => {
-  lemon.draggable(false); // stop dragging when released
+  lemon.draggable(false);
   lemon.scale({x: 1, y: 1});
+  animation.stop();
+  lemon.rotation(og_orientation);
 });
 
 
 
-// add the shape to
-// the layer
-//layer.add(circle);
+// add the shape to the layer
 layer.add(lemon);
 
 // add the layer to the stage
 stage.add(layer);
-
